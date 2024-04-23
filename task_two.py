2c578a2d013f17152718cd389e919240cd7fd5ee
Anton <korniyenko84@gmail.com>
2024-04-23 16:57:57 +0300

    task_two

--- task_two.py
+++ task_two.py
@@ -3,17 +3,17 @@ for a lottery ticket."""
 
 import random
 
-def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
+def get_numbers_ticket(min_val: int, max_val: int, quantity: int) -> list[int]:
     """This function generates a list of unique and sorted random numbers for a lottery ticket."""
     numbers = set()
-    if max - min + 1 < quantity:
+    if max_val - min_val + 1 < quantity:
         print("Error: Quantity is larger than the range of numbers or equal!")
         return sorted(numbers)
-    if not 1 <= min <= max <= 1000:
+    if not 1 <= min_val <= max_val <= 1000:
         print("Error: Invalid input. Ensure 1 <= min <= max <= 1000!")
         return sorted(numbers)
     while len(numbers) < quantity:
-        numbers.add(random.randint(min, max))
+        numbers.add(random.randint(min_val, max_val))
     return sorted(numbers)
 
 # Test the function
